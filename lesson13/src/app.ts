64ce3a241eb6f0536cd33b1618f4180c0f3f8b6e
LLpanov <leonardopanov@yahoo.com>
2022-11-02 19:14:26 +0200

    /socketIO

--- lesson13/src/app.ts
+++ lesson13/src/app.ts
@@ -12,20 +12,54 @@ const app = express();
 const server = http.createServer(app);
 
 // @ts-ignore
-const io = SocketIO(server,{cors:{origin:'*'}});
+const io = SocketIO(server, {cors: {origin: '*'}});
 
-io.on('connection',(socket:any)=>{
-    console.log('--------------------')
+io.on('connection', (socket: any) => {
+
     console.log(socket.handshake.query);
-    console.log('-----------------------')
-})
+    socket.on('message:create', (data: any) => {
+        console.log(data);
+        //one to one
+        // socket.emit('message:get-all', {message: [{text: data.message}]})
+
+        ///send all online users
+        // io.emit('message:get-all', {message: [{text: data.message}]})
+
+       //send all users avoid sender
+        socket.broadcast.emit('message:get-all', 'Test');
+    })
+
+    socket.on('join_room', (data: any) => {
+        socket.join(data.id)
+        //emmit to one user's
+        // socket.broadcast.to(data.id).emit(`user_join_room`,{message:`User ${socket.id} joined to room ${data.id}`})
+        //emit to all users in room include sender
+        io.to(data.id).emit(`user_join_room`, {message: `User ${socket.id} joined to room ${data.id}`})
+    })
+/// 1-1
+    //socket.emit()
+
+    // all include sender
+    //io.emit()
+
+    // get all users avoid sender
+    //socket.broadcast.emit()
+
+    // to room avoid sender
+    //socket.broadcast.to(room_id).emit
+
+    //emit to all users in room include sender
+    // io.to(room_id).emit()
+
+
+});
 
 app.use(fileUpload());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(apiRouter);
 
-const { PORT } = process.env;
+const {PORT} = process.env;
 
 // @ts-ignore
 app.use('*', (err, req, res, next) => {
@@ -46,7 +80,7 @@ server.listen(process.env.PORT, async () => {
             console.log('database connect');
             // await cronRun();
         }
-    } catch ({ message }) {
+    } catch ({message}) {
         // eslint-disable-next-line no-console
         console.log(message);
     }
